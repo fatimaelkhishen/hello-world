18deb68155211fdf75317355f1b3792ce8dd7d99
fatimaelkhishen <fatima.al-khishen@hotmail.com>
2024-09-04 17:56:09 +0300

    Eaglejobs

--- Eaglejobs.py
+++ Eaglejobs.py
@@ -69,7 +69,7 @@ def construct_job(driver, job_link):
         salary = "NA"
         
     try:
-        description = soup.find('div', class_='awsm-job-entry-content entry-content').text
+        description = soup.find('div', id='formToggleSheet').text
     except:
         description = "NA"
     try:
